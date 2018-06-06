4f0cc88e33fd577fe4eb020048f398a158c8dc9d
Wilkinson <tam@tanweixiongmac.local>
2018-06-06 16:42:51 +0800

    新增分页功能

--- PTNPurse/Comom(常用)/GlobalVariables(全局变量)/ZYConstAPIURL.swift
+++ PTNPurse/Comom(常用)/GlobalVariables(全局变量)/ZYConstAPIURL.swift
@@ -12,7 +12,7 @@ struct ZYConstAPI {
 
 //    static let kAPIH5BaseURL: String = "http://api.ptn.one:8080"
 //    static let kAPIBaseURL: String = "http://api.ptn.one:8080/"  //正式服务
-    static let kAPIBaseURL: String = "http://192.168.20.24:8080/"  //正聪服务
+    static let kAPIBaseURL: String = "http://192.168.20.3:8080/"  //正聪服务
 //    static let kAPIBaseURL: String = "http://47.92.165.25:8080/"  //测试服务
 
     
